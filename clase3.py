c71cda2d67e0c1a25da1c573b3998a64dcc2b8b0
Lucas Rodriguez <xlucasrodriguez@gmail.com>
2023-09-20 20:17:32 -0300

    Otro cambio

--- clase3.py
+++ clase3.py
@@ -2,6 +2,11 @@
 
 #INSTALARLO - Chequeando que este tildado GIT BASH y GIT GUI, Seleccionando Visual Studio Code as default editor - El resto todo next
 
+#Configuramos GIT con nuestro nombre escribiendo: git config --global user.name "Nuestro nombre"
+#Configuramos GIT con nuestro email escribiendo: git config --global user.email micorreo@correo.com
 
-#Identificamos 3 estados - Working Directory -> Staging Area -> Repository
+#Abrimos la terminal en el VSC y luego abrimos la consola de Git Bash
 
+#Iniciamos git escribiendo: git init
+
+# En GIT podemos identificar 3 estados - Working Directory -> Staging Area -> Repository
