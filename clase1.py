642eaca4f68178d700f2e37d7769785d354f6d57
Lucas Rodriguez <xlucasrodriguez@gmail.com>
2023-09-20 19:31:45 -0300

    Subiendo el boton del login

---
+++ clase1.py
@@ -0,0 +1,23 @@
+"""
+
+mi_nombre = "Coc"
+
+print(mi_nombre)"""
+
+"""Esto es una suma"""
+"""print(8+8)
+
+mi_nombre = "Lucas"
+
+mi_apellido = "Rodriguez"
+
+
+
+"""
+""" notas_del_examen1 = int(input("Cual es la nota de tu primer parcial?"))
+
+notas_del_examen2 = int(input("Cual es la nota de tu segundo parcial?"))
+
+resultado = (notas_del_examen1 + notas_del_examen2) / 2
+
+print(resultado) """
---
+++ clase2.py
@@ -0,0 +1,38 @@
+
+#Las listas se tratan de un tipo compuesto de dato que puede almacenar distintos valores (llamados ítems o elementos) ordenados entre [ ] y separados con comas:
+
+""" numeros = [1,2,3,4]
+print(numeros)
+
+
+ """
+""" 
+#Listas mutables: A diferencia de las cadenas, en las listas sí podemos modificar sus ítems utilizando índices:
+
+pares = [0,2,4,5,8,10]
+pares[3] = 6
+
+
+print(pares)
+ """
+
+#Funcionan de una forma muy similar a las cadenas de caracteres:
+
+""" datos = [4, "Una cadena", -15, 3.14, "Otra cadena"]
+
+print(datos[0])
+print(datos[-1])
+print(datos[2:])
+ """
+#Suma de listas
+
+""" print(numeros + [5,6,7,8])
+
+a = [1,2,3]
+b = [4,5,6]
+c = [7,8,9]
+r = [a,b,c]
+
+print(r) """
+
+print(5+115)
