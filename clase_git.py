82629fd13f19ba2b4ac94b3501118d1947fd1b13
Lucas Rodriguez <xlucasrodriguez@gmail.com>
2023-09-27 21:08:43 -0300

    Clase Subida a Github

---
+++ clase_git.py
@@ -0,0 +1,19 @@
+""" 
+git init                      // INICIAMOS GIT
+git add .                       // AGREGAMOS LOS CAMBIOS AL STAGING AREA 
+git commit -m "motivo del commit" // COMMITEAMOS LOS CAMBIOS 
+git remote add origin https://github.com/nombre_usuario/nombre_repo // AGREGAMOS EL REPO
+git push -u origin master // PUSHEAMOS EL REPO
+
+
+ALTERNATIVA PARA TRANSFORMAR LA RAMA MASTER A MAIN
+git init
+git add .
+git commit -m "commit"
+git branch -M main // ESTA ACCION TRANSFORMA LA RAMA MASTER A MAIN
+git remote add origin https://github.com/lucasxkent1947/REPO1.git
+git push -u origin main   // POR ESO ACÁ CAMBIAMOS DE MASTER A MAIN
+
+
+
+"""
